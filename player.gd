4da1f892b640c5d65be895d5554e959546999379
Marcin <marcinburek16@gmail.com>
2026-06-14 22:03:56 +0200

    0.4 Rewrote controls, made new collisions from scratch and drew a new accurate tileset

--- player.gd
+++ player.gd
@@ -1,29 +1,52 @@
 extends CharacterBody2D
+class_name Player
 
-const max_speed := 40.0
-const min_speed := 8.0
-const time_to_max_speed := 0.1
-const time_to_stop := 0.05
-const time_to_turn := 0.05
+@export var speed: float = 80.0
 
-const acceleration := max_speed / time_to_max_speed
-const friction := max_speed / time_to_stop
-const turn_speed := max_speed / time_to_turn
+func _physics_process(_delta: float) -> void:
+	var raw_direction := Vector2.ZERO
 
-var dir_input := Vector2.ZERO
+	# --- DIAGONALS FIRST ---
+	if Input.is_action_pressed("action_up") and Input.is_action_pressed("action_right"):
+		raw_direction = Vector2(1, -1)
+		$AnimatedSprite2D.frame = 1
 
-func _physics_process(delta: float) -> void:
-	dir_input = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	print("working")
-	if dir_input != Vector2.ZERO:
-		if !dir_input.normalized().is_equal_approx(velocity.normalized()):
-			velocity += dir_input * turn_speed * delta
-		else:
-			velocity += dir_input * acceleration * delta
-		if velocity.length() > max_speed:
-			velocity = velocity.normalized() * max_speed
-	elif velocity != Vector2.ZERO:
-		velocity -= velocity.normalized() * friction * delta 
-		if velocity.length() < min_speed:
-			velocity = Vector2.ZERO
+	elif Input.is_action_pressed("action_right") and Input.is_action_pressed("action_down"):
+		raw_direction = Vector2(1, 1)
+		$AnimatedSprite2D.frame = 3
+
+	elif Input.is_action_pressed("action_down") and Input.is_action_pressed("action_left"):
+		raw_direction = Vector2(-1, 1)
+		$AnimatedSprite2D.frame = 5
+
+	elif Input.is_action_pressed("action_left") and Input.is_action_pressed("action_up"):
+		raw_direction = Vector2(-1, -1)
+		$AnimatedSprite2D.frame = 7
+
+	# --- CARDINAL DIRECTIONS ---
+	elif Input.is_action_pressed("action_up"):
+		raw_direction = Vector2(0, -1)
+		$AnimatedSprite2D.frame = 0
+
+	elif Input.is_action_pressed("action_right"):
+		raw_direction = Vector2(1, 0)
+		$AnimatedSprite2D.frame = 2
+
+	elif Input.is_action_pressed("action_down"):
+		raw_direction = Vector2(0, 1)
+		$AnimatedSprite2D.frame = 4
+
+	elif Input.is_action_pressed("action_left"):
+		raw_direction = Vector2(-1, 0)
+		$AnimatedSprite2D.frame = 6
+
+
+	# Animation
+	if raw_direction != Vector2.ZERO:
+		$AnimatedSprite2D.play()
+	else:
+		$AnimatedSprite2D.pause()
+
+	# Movement
+	velocity = raw_direction.normalized() * speed
 	move_and_slide()
