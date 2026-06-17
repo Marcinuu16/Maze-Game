d1940f5474fa01ec0babf6b744ef0557fc540d49
Marcin <marcinburek16@gmail.com>
2026-06-17 21:56:42 +0200

    0.5 Fully working modular room swapping

--- scripts/door_trigger.gd
+++ scripts/door_trigger.gd
@@ -2,9 +2,12 @@ extends Area2D
 
 enum Target_room_e {r1,r2,r3,r4,r5,r6,r7,r8,r9,r10,r11,r12}
 enum Target_door_e {d1,d2,d3,d4,d5,d6}
+enum Target_direction_e {north,south,east,west}
+
 @export var Target_room = Target_room_e.r1
 @export var Target_door = Target_door_e.d1
+@export var Target_direction = Target_direction_e.north
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	connect("body_entered", Callable(room_manager, "swap_room").bind(Target_room+1,Target_door+1))
+	connect("body_entered", Callable(room_manager, "prepare_swap").bind(Target_room+1,Target_door+1,Target_direction))
 	
--- scripts/room_manager.gd
+++ scripts/room_manager.gd
@@ -1,5 +1,5 @@
 extends Node
-
+var is_swapping = false
 var current_room: Node = null
 var player: Node = null
 # Called when the node enters the scene tree for the first time.
@@ -16,19 +16,50 @@ func _process(_delta: float) -> void:
 
 func _input(event):
 	if event.is_action_pressed("action_debug"):
-		print("debug kliknięty")
+		print("debug button clicked")
 			
-func swap_room(body, to_room, to_door):
+func prepare_swap(body, to_room, to_door,to_direction):
+	if body is CharacterBody2D:
+		if is_swapping:
+			print("swap blocked")
+			return
+		is_swapping = true
+		print("ready to swap")
+		call_deferred("swap_room",body,to_room,to_door,to_direction)
+	
+func swap_room(body, to_room, to_door,to_direction):
 	if body is CharacterBody2D:
 		print("Trying to enter room "+str(to_room)+", door "+str(to_door))
+		player.reparent(get_node("/root/room_manager"),true)
 		
 		if current_room:
-			current_room.get_node("TileMap").remove_child(player)
 			current_room.queue_free()
+			print("deleted room")
 			
-			player = load("res://scenes/player.tscn").instantiate()
-			current_room = load("res://scenes/room"+str(to_room)+".tscn").instantiate()
-			get_node("/root/game").add_child(current_room)
-			current_room.get_node("TileMap").add_child(player)
-			print("loaded room"+str(to_room))
+		current_room = load("res://scenes/room"+str(to_room)+".tscn").instantiate()
+		get_node("/root/game").add_child(current_room)
+		player.reparent(current_room.get_node("TileMap"),true)
+		var spawn_cords: Vector2 = Vector2(0,0)
+		match to_direction:
+			0:
+				spawn_cords = Vector2(-16,-16)
+				print("door leading north")
+			1:
+				print("door leading south")
+				spawn_cords = Vector2(16,16)
+			2:
+				print("door leading east")
+				spawn_cords = Vector2(16,-16)
+			3:
+				print("door leading west")
+				spawn_cords = Vector2(-16,8)
+				
+		print(player.position)
+		print(current_room.get_node("door"+str(to_door)).position)
+		print(spawn_cords)
+		player.position = current_room.get_node("door"+str(to_door)).position + spawn_cords
+		
+		print("loaded room "+str(to_room))
+		is_swapping = false
+		
 		
