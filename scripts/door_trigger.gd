b44be309f313bfc4c0fd8d69a46088aee9229788
Marcin <marcinburek16@gmail.com>
2026-06-17 08:38:22 +0200

    0.4.3 Pseudo working automatic room swapping

--- scripts/door_trigger.gd
+++ scripts/door_trigger.gd
@@ -1,10 +1,10 @@
 extends Area2D
 
-enum Target_room_e {r0,r1,r2,r3,r4,r5,r6,r7,r8,r9,r10,r11}
-enum Target_door_e {d0,d1,d2,d3,d4}
-@export var Target_room = Target_room_e.r0
-@export var Target_door = Target_door_e.d0
+enum Target_room_e {r1,r2,r3,r4,r5,r6,r7,r8,r9,r10,r11,r12}
+enum Target_door_e {d1,d2,d3,d4,d5,d6}
+@export var Target_room = Target_room_e.r1
+@export var Target_door = Target_door_e.d1
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	connect("body_entered", Callable(room_manager, "on_trigger_entered").bind(Target_room,Target_door))
+	connect("body_entered", Callable(room_manager, "swap_room").bind(Target_room+1,Target_door+1))
 	
--- scripts/room_manager.gd
+++ scripts/room_manager.gd
@@ -1,8 +1,14 @@
 extends Node
 
+var current_room: Node = null
+var player: Node = null
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	player = load("res://scenes/player.tscn").instantiate()
+	current_room = load("res://scenes/room1.tscn").instantiate()
+	get_node("/root/game").add_child(current_room)
+	current_room.get_node("TileMap").add_child(player)
+	print("loaded room 1")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
@@ -11,14 +17,18 @@ func _process(_delta: float) -> void:
 func _input(event):
 	if event.is_action_pressed("action_debug"):
 		print("debug kliknięty")
-		var room = preload("res://scenes/room1.tscn").instantiate()
-		get_node("/root/game").add_child(room)
-		
-		var player= preload("res://scenes/player.tscn").instantiate()
-		room.get_node("TileMap").add_child(player)
-		
-	
-		
-func on_trigger_entered(body, room, door):
+			
+func swap_room(body, to_room, to_door):
 	if body is CharacterBody2D:
-		print("Trying to enter "+str(room)+"'s door nr "+str(door))
+		print("Trying to enter room "+str(to_room)+", door "+str(to_door))
+		
+		if current_room:
+			current_room.get_node("TileMap").remove_child(player)
+			current_room.queue_free()
+			
+			player = load("res://scenes/player.tscn").instantiate()
+			current_room = load("res://scenes/room"+str(to_room)+".tscn").instantiate()
+			get_node("/root/game").add_child(current_room)
+			current_room.get_node("TileMap").add_child(player)
+			print("loaded room"+str(to_room))
+		
